03a3329842b5ffe0f3ae64253fbfe3cb703a02a0
jjaykim <dnglglmm@gmail.com>
2021-03-31 13:37:24 -0400

    #3. Longest Substring Without Repeating Characters

---
+++ Algorithm/LeetCode/Longest_Substring/sliding_window.cpp
@@ -0,0 +1,49 @@
+// using sliding window algorithm and tow pointer
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+int lengthOfLongestSubstring(std::string s) {
+  // using unordered_set
+  /*
+  int result = 0;
+  unordered_set<char> arr;
+  int left = 0;
+  int right = 0;
+
+  while (left < s.size() && right < s.size()) {
+    if (arr.find(s[right]) == arr.end()) {
+      arr.insert(s[right]);
+      result = max(result, right - left + 1);
+      right++;
+    }
+    else {
+      arr.erase(s[left]);
+      left++;
+    }
+  }
+
+  return result;
+  */
+
+  if (s.size() == 0) return 0;
+
+  int arr[128] = {0};
+  int max = 0, left = 0, right = 0;
+
+  for (int i = 0; i < s.size(); i++) {
+    arr[int(s[i])]++;
+    right++;
+
+    while (arr[int(s[i])] > 1) {
+      arr[int(s[left])]--;
+      left++;
+      right--;
+    }
+
+    if (right > max)
+      max = right;
+  }
+
+  return max;
+}
