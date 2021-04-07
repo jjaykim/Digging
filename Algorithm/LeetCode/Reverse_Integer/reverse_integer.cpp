117c6ac8e15235ff405974f5a0d47f7731edb1cc
jjaykim <dnglglmm@gmail.com>
2021-04-06 21:07:55 -0400

    #7.Reverse Integer

---
+++ Algorithm/LeetCode/Reverse_Integer/reverse_integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+  int reverse(int x) {
+    long result = 0;
+
+    while (x != 0) {
+      result = result * 10 + x % 10;
+      x = x / 10;
+    }
+
+    if (result > INT_MAX || result < INT_MIN) result = 0;
+    // INT_MAX and INT_MIN are included in <limits.h>
+
+    return result;
+  }
+};
