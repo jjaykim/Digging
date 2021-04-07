5658b4010bffba681bdbbfc60ccedc0854241c2d
jjaykim <dnglglmm@gmail.com>
2021-04-06 21:11:48 -0400

    #9. Palindrome_Number

--- Algorithm/LeetCode/Palindrome_Number/Palindrome_Number.cpp
+++ Algorithm/LeetCode/Palindrome_Number/Palindrome_Number.cpp
@@ -1,17 +1,19 @@
+//  https://leetcode.com/problems/palindrome-number/
+
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        long _first = x;
-        long _check = 0;
+  bool isPalindrome(int x) {
+    long _first = x;
+    long _check = 0;
 
+    if (x < 0)
+      return false;
 
-        if (x < 0) return false;
-
-        while (x != 0) {
-            _check = _check * 10 + x % 10;
-            x = x / 10;
-        }
-
-        return _first == _check;
+    while (x != 0) {
+      _check = _check * 10 + x % 10;
+      x = x / 10;
     }
+
+    return _first == _check;
+  }
 };
